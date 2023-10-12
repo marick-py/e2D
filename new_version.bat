a87378b727802a9e2d96f99d709accacf03a574b
Riko Mari <ricomari2006@gmail.com>
2023-10-12 03:10:34 +0200

    new version

--- new_version.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-python new_version.py
-python -m build
-python -m twine upload -u ma.rick --skip-existing --repository pypi dist/*
-pause
--- new_version.py
+++
@@ -1,9 +0,0 @@
-import configparser as cp
-cfg = cp.ConfigParser()
-_ = cfg.read("setup.cfg")
-current_version = cfg.get("metadata", "version")
-new_version = input(f"Current version: [{current_version}]\n>>> ")
-if new_version != "":
-    cfg.set("metadata", "version", new_version)
-with open("setup.cfg", "w") as config_file:
-    cfg.write(config_file)
