896533582753c7d27a2781b0c400426119d877a2
Riccardo Mariani <ricomari2006@gmail.com>
2026-02-27 11:51:30 +0100

    chore: Bump version to 2.1.8 in __init__.py, setup.cfg, README.md; update copy_from methods in Vector2D and Vector2Int for correct type handling

--- e2D/cvectors.pyx
+++ e2D/cvectors.pyx
@@ -88,10 +88,10 @@ cdef class Vector2D:
         self._data_ptr[1] = y
         return self
     
-    cpdef Vector2D copy_from(self, Vector2Int other):
+    cpdef Vector2D copy_from(self, Vector2D other):
         """Copy components from another vector (int to float)"""
-        self._data_ptr[0] = <double>other._data_ptr[0]
-        self._data_ptr[1] = <double>other._data_ptr[1]
+        self._data_ptr[0] = other._data_ptr[0]
+        self._data_ptr[1] = other._data_ptr[1]
         return self
     
     cpdef Vector2D iadd(self, Vector2D other):
@@ -736,10 +736,10 @@ cdef class Vector2Int:
         self._data_ptr[1] = y
         return self
     
-    cpdef Vector2Int copy_from(self, Vector2D other):
+    cpdef Vector2Int copy_from(self, Vector2Int other):
         """Copy components from another vector (float to int, truncates)"""
-        self._data_ptr[0] = <int>other._data_ptr[0]
-        self._data_ptr[1] = <int>other._data_ptr[1]
+        self._data_ptr[0] = other._data_ptr[0]
+        self._data_ptr[1] = other._data_ptr[1]
         return self
     
     cpdef Vector2Int iadd(self, Vector2Int other):
